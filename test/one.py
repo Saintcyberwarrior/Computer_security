00121cdc35515d08edc6f12d6278681b896eb642
Yash Vardhan Bhobia <yvbhobia@gmail.com>
2022-09-24 16:37:42 +0530

    nothing new

--- test/one.py
+++ test/one.py
@@ -17,7 +17,32 @@ state = p.factory.entry_state()
 sm = p.factory.simulation_manager(state)
 
 #Put a condition on program to stop once it encounter a syscall
-sm.run(until=lambda sm: sm.is_simprocedure==True)
+#sm.run(until=lambda sm: sm.is_simprocedure==True)
 
 #condition to check if there is any branching
+
+#TODO
+#there might be braches while traversing the graph in reverse direction, need to take care of that case as well.
 sm.run(until=lambda sm_:len(sm_.active)>1)
+
+def syscall_func(state):
+	print('the syscall made now was', state.inspect.syscall_name)
+
+def call_func(state):
+	print('the address of function called is: ', state.inspect.function_address)
+
+def ret_func(state):
+	print('Wohoo, we just returned')
+
+def exit_func(state):
+	print('the exit target is', state.inspect.exit_target, 'and the exit guard is', state.inspect.exit_guard, 'and the jumpkind is', state.inspect.exit_jumpkind)
+
+##I have no idea what guard is#
+state.inspect.b('syscall', when=angr.BP_BEFORE, action = syscall_func)# here we can add condition function also
+state.inspect.b('call', when=angr.BP_BEFORE, action = call_func)
+state.inspect.b('return', when=angr.BP_BEFORE, action = ret_func)
+state.inspect.b('exit', when=angr.BP_BEFORE, action = exit_func)
+
+##############################So, so this way we can isolate the states of our interest############
+
+##Now, put them in a loop###
